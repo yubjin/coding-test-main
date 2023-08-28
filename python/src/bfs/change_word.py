239b4a577ac665e606d34955e02415d4ccaa4969
Yubin Shin <116477922+yubjin@users.noreply.github.com>
2023-08-28 17:29:05 +0900

    Update change_word.py
    단어 변환 문제 해결

--- python/src/bfs/change_word.py
+++ python/src/bfs/change_word.py
@@ -1,3 +1,19 @@
+from collections import deque
+
 def change_word(begin,target,words):
-    
-    pass
+    #초기화
+    queue = deque()
+    queue.append([begin, 0])
+    visitied = [0] * len(words)
+
+    #계산
+    while queue:
+        word, cnt = queue.popleft()
+        if word == target: return cnt
+        for i in range(len(words)):
+            if not visitied[i]:
+                if sum(x != y for x,y in zip(word,words[i])) ==1:
+                    queue.append(words[i], cnt +1)
+                    visitied = 1
+    #결과
+    return 0
