f6263780f36b6058467b2e75028c1d5a6fef21d0
Yubin Shin <116477922+yubjin@users.noreply.github.com>
2023-08-28 17:58:51 +0900

    최단경로 문제
    프로그래머스 최단경로 문제(가장 먼  노드) 해결

--- python/tests/test_bfs.py
+++ python/tests/test_bfs.py
@@ -1,8 +1,11 @@
 import unittest
 from src.bfs.change_word import change_word
+from src.bfs.shortest import shortest
 
 
 class TestBfs(unittest.TestCase):
     def test_change_word(self):
         self.assertEqual(change_word("hit","cog",["hot", "dot", "dog", "lot", "log", "cog"]),4)
         self.assertEqual(change_word("hit","cog",["hot", "dot", "dog", "lot", "log"]),0)
+    def test_short(self):
+        self.assertEqual(shortest(6,[[3,6],[4,3],[3,2],[1,3],[1,2],[2,4],[5,2]]),3)
